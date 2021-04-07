6c201c3e3c6d4609344203d4c88538300f3c9aae
Florian Maurer <flmaurer@icloud.com>
2021-04-07 15:30:16 +0200

    Finished Task 3

--- assignment2/src/Analyzing.py
+++ assignment2/src/Analyzing.py
@@ -1,10 +1,33 @@
 import pandas as pd
+import numpy
+
+def to_float(x):
+    try:
+        x = float(x)
+    except:
+        x = numpy.nan
+    return x
+
 
 if __name__ == '__main__':
+    df = pd.read_csv("../movies_metadata.csv", low_memory=False)
+    print(df.tail(1).values)
+    print(df.head(1).values)
 
-    df = pd.read_csv("../movies_metadata.csv",low_memory=False)
-    print (df.get("overview")[0])
-    print(df.get("overview")[len(df)-1])
+    getJumanji = df.loc[df['original_title'] == "Jumanji"]
 
-    #Show the Infromation of Jumanji..
+    print (getJumanji.values)
+
+
+    small_df = df[['title', 'release_date', 'popularity', 'revenue', 'runtime',
+    'genres']].copy()
+    small_df.loc['release_date'] = pd.to_datetime(small_df['release_date'],
+    errors='coerce')
+    small_df['release_year'] = small_df['release_date'].apply(lambda x: str(x).split('-')[0] if x != numpy.nan else numpy.nan)
+    small_df['release_year'] = small_df['release_year'].apply(to_float)
+    small_df['release_year'] = small_df['release_year'].astype('float')
+    small_df = small_df.drop(columns="release_date")
+
+    youngMovies = small_df[small_df['release_year'] >= 2010]
+    print(youngMovies)
 
