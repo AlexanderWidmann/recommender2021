be43537e8a4fe0740af32f0f414b3afccb5c45e0
Alexander Widmann <alexanderwi@edu.aau.at>
2021-04-26 09:59:41 +0200

    Reworked Recommender

--- assignment4/content_based_.py
+++ assignment4/content_based_.py
@@ -6,6 +6,8 @@ path = "./MovieLens_1M/"
 movie_header = ['MovieId', 'Title', 'Genre']
 ratings_header = ['UserId', 'MovieId', 'Ratings', 'Timestamp']
 user_header = ['UserId', 'Gender', 'Age', 'Occupation', 'ZipCode']
+score_string = 'Score'
+overlap_string = 'Overlap'
 
 genre_counter_header = ['Genre', 'Amount']
 
@@ -16,11 +18,14 @@ pd.set_option('display.max_rows', 1000)
 def get_data(id):
     # needed to properly import the MovieLens-1M Dataset
     if id == 0:
-        return pd.read_csv(path + 'movies.dat', sep='::', engine='python', header=None, names=movie_header)
+        return pd.read_csv(path + 'movies.dat', sep='::', engine='python', header=None, names=movie_header,
+                           index_col=movie_header[0])
     elif id == 1:
-        return pd.read_csv(path + 'ratings.dat', sep='::', engine='python', header=None, names=ratings_header)
+        return pd.read_csv(path + 'ratings.dat', sep='::', engine='python', header=None, names=ratings_header,
+                           index_col=ratings_header[0])
     elif id == 2:
-        return pd.read_csv(path + 'users.dat', sep='::', engine='python', header=None, names=user_header)
+        return pd.read_csv(path + 'users.dat', sep='::', engine='python', header=None, names=user_header,
+                           index_col=user_header[0])
     else:
         return
 
@@ -42,7 +47,7 @@ def get_user_id() -> int:
 def show_first_fifteen_movies(userid):
     # Select the movies which where rated by the given user
     # In SQL it would be SELECT * where user_id = x
-    current_user = ratingsDf[ratingsDf[user_header[0]] == userid]
+    current_user = ratingsDf.loc[userid]
     # then merge with the movie-data to get the movie-information
     current_user_movies = current_user.merge(moviesDf, how='inner', on=movie_header[0])
     # and print the first 15 movies (only the title and the genre without index)
@@ -54,17 +59,13 @@ def show_first_fifteen_movies(userid):
 
 def countGenres():
     # In SQL it would be SELECT * where user_id = x
-    current_user = ratingsDf[ratingsDf[user_header[0]] == userid]
-    # then merge with the movie-data to get the movie-information
-    current_user_movies = current_user.merge(moviesDf, how='inner', on=movie_header[0])
-
-    # Get every genre which were rated higher than 3
-    genre_count = current_user_movies[current_user_movies[ratings_header[2]] > 3]
+    current_user = ratingsDf.loc[userid]
+    # Create a true/false matrix for all movies, true if the user liked it
+    liked_movies = current_user[current_user[ratings_header[2]] > 3]
+    # filter the genreDf to only accept the liked movies
+    genre_count = genreDf.loc[liked_movies[movie_header[0]]]
     # counted the genres and added to new dataframe
-    genre_count = genre_count[genre_counter_header[0]].value_counts().to_frame(genre_counter_header[1]).reset_index()
-
-    # renamed the column index to genre
-    genre_count = genre_count.rename(columns={"index": genre_counter_header[0]})
+    genre_count = genre_count.sum(axis=0, skipna=True)
 
     print(genre_count)
 
@@ -76,64 +77,63 @@ def countGenres():
 # genre which were rated by the user.
 # Additional, the values will be sorted by the Amount of the ratings.
 def simple_content_based():
-    # get the genres from the user.
-    genre_count = countGenres()
-    # Get the Movies from the genres which were rated by the given user.
-    recommended_movies = moviesDf.merge(genre_count, how="inner", on="Genre")
-
+    # filter movies the user already rated
+    current_user = ratingsDf.loc[userid]
     # sort the movies by the amount of the rated genres.
-    recommended_movies = recommended_movies.sort_values(by='Amount', ascending=False)
-
-    # print the first 15 Movies from the DataFrame.
-
-    print("******* SIMPLE CONTENT BASED PREDICTION ****** ")
-
-    print(recommended_movies[['Title', 'Genre']])
-
+    recommended_movies = moviesDf.query('MovieId not in @current_user')
+    recommended_movies[overlap_string] = calculate_overlap()
+    recommended_movies[score_string] = recommended_movies[overlap_string]
     return recommended_movies
 
 
-def get_non_overlaped_Movies ():
-    #Get the rated Movies
-    current_user = ratingsDf[ratingsDf[user_header[0]] == userid]
-    # then merge with the movie-data to get the movie-information
-    current_user_movies = current_user.merge(moviesDf, how='inner', on=movie_header[0])
+def simple_content_based_with_popularity():
+    recommended_movies = simple_content_based()
+    recommended_movies[score_string] = get_popularity()
+    recommended_movies = recommended_movies.loc[recommended_movies[overlap_string] > 0]
+    return recommended_movies
 
-    #This two rows, do an Right Outer Join to get all movies which were not rated by the given user
-    not_rated_Movies = pd.merge(current_user_movies, moviesDf, on=[movie_header[0]], how="outer", indicator=True)
 
-    not_rated_Movies = not_rated_Movies[not_rated_Movies['_merge'] == "right_only"]
+def get_popularity():
+    return ratingsDf[[ratings_header[1], ratings_header[2]]].groupby(movie_header[0]).sum()
 
-    return not_rated_Movies
 
+def calculate_overlap():
+    genre_count = countGenres()
+    # filter the genreDf based on the userprofile
+    filtered_genreDf = genreDf.loc[:, genre_count > 0]
+    return filtered_genreDf.sum(axis='columns')
 
 
 def extended_content_based():
-    # get the movies which were not rated by the given user
-    recommended_movies = get_non_overlaped_Movies()
-
-    # make an inner join with the ratings dataframe, so we can count the amount of ratings for each movie, that has
-    # not been rated by the user
-    recommended_movies = recommended_movies.merge(ratingsDf, on=movie_header[0], how="inner")
-
-    # count the ratings
-    count = pd.DataFrame(recommended_movies[movie_header[0]].value_counts().reset_index())
-
-    count.columns = [movie_header[0], "rating_count"]
-
-    i = pd.merge(count, moviesDf, how="inner", on=movie_header[0]).sort_values(by="rating_count", ascending=False)
-
-    print(i[[movie_header[0], movie_header[1], movie_header[2], 'rating_count']].head(10))
-
+    # filter movies the user already rated
+    current_user = ratingsDf.loc[userid]
+    # sort the movies by the amount of the rated genres.
+    recommended_movies = moviesDf.query('MovieId not in @current_user')
+    weighted_genres = genreDf * countGenres()
+    # replace 0 with nan for easier maths
+    weighted_genres = weighted_genres.replace(0, np.nan).mean(axis='columns')
+    recommended_movies['Popularity'] = get_popularity()
+    # multiply the popularity with the squared genre-weight (and divide by 1000 to keep the numbers smaller
+    recommended_movies[score_string] = recommended_movies['Popularity'] * (weighted_genres/100) ** 2
+    return recommended_movies
 
 
 if __name__ == '__main__':
     moviesDf = pd.DataFrame(get_data(0))
     ratingsDf = pd.DataFrame(get_data(1))
     usersDf = pd.DataFrame(get_data(2))
-
+    # create a df that splits the genre column in the seperate columns
+    genreDf = moviesDf.copy().pop(movie_header[2]).str.get_dummies(sep='|')
     userid = get_user_id()
 
     # print the first 15 movies for the selected user
-    show_first_fifteen_movies(userid)
-    extended_content_based()
+
+    # show_first_fifteen_movies(userid)
+
+    # here are the 3 algorithms
+    # rec = simple_content_based()
+    # rec = simple_content_based_with_popularity()
+    rec = extended_content_based()
+
+    rec = rec.sort_values(by=score_string, ascending=False)
+    print(rec.head(10))
