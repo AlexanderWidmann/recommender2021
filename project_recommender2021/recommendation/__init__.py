c1a35a065cd1660139586280fba010901c313357
Florian Maurer <flmaurer@icloud.com>
2021-05-31 19:39:04 +0200

    made a little method which create smaller csv.

--- project_recommender2021/recommendation/__init__.py
+++ project_recommender2021/recommendation/__init__.py
@@ -5,52 +5,70 @@ from pathlib import Path
 import pandas as pd
 
 pd.set_option('display.max_rows', 1000)
-pd.set_option('display.max_columns', 30)
+pd.set_option('display.max_columns', 20)
 
-movie_header = ['MovieId', 'Title', 'Genre']
-ratings_header = ['UserId', 'MovieId', 'Ratings', 'Timestamp']
-user_header = ['UserId', 'Gender', 'Age', 'Occupation', 'ZipCode']
+movie_header = ['movieId', 'title', 'genre']
+ratings_header = ['userId', 'movieId', 'ratings', 'timestamp']
+genome_scores_header = ['movieId', 'tagId', 'relevance']
+genome_tags_header = ['tagId', 'tag']
+tags_header = ['userId,movieId,tag,timestamp']
 
 p = Path(__file__).parent
-path = p.joinpath('MovieLens_1M')
+path = p.joinpath('ml-25m')
 
 
 def get_data(id):
     # needed to properly import the MovieLens-1M Dataset
     if id == 0:
-        return pd.read_csv(path.joinpath('movies.dat'), sep='::', engine='python', header=None, names=movie_header)
+        return pd.read_csv(path.joinpath('movies.csv'), engine='python')
     elif id == 1:
-        return pd.read_csv(path.joinpath('ratings.dat'), sep='::', engine='python', header=None, names=ratings_header)
+        return pd.read_csv(path.joinpath('ratings.csv'), engine='python')
     elif id == 2:
-        return pd.read_csv(path.joinpath('users.dat'), sep='::', engine='python', header=None, names=user_header)
+        return pd.read_csv(path.joinpath('genome-scores.csv'), engine='python')
     elif id == 3:
-        return pd.read_csv(path.joinpath('movies_metadata.csv'), engine='python')
+        return pd.read_csv(path.joinpath('genome-tags.csv'), engine='python')
+    elif id == 4:
+        return pd.read_csv(path.joinpath('tags.csv'), engine='python')
+
     else:
         return
 
 
-moviesDf = pd.DataFrame(get_data(0))
-ratingsDf = pd.DataFrame(get_data(1))
-usersDf = pd.DataFrame(get_data(2))
-metaDf = pd.DataFrame(get_data(3))
-metaDf = metaDf.rename(columns={"id": movie_header[0], "title": movie_header[1]})
-metaDf = metaDf[metaDf[movie_header[0]].apply(lambda x: str(x).isdigit())]
-metaDf[movie_header[0]] = metaDf[movie_header[0]].astype(int)
+moviesDf = pd.DataFrame(get_data(0)).head(1000)
+ratingsDf = pd.DataFrame(get_data(1)).head(1000)
+genome_scoresDf = pd.DataFrame(get_data(2)).head(1000)
+genome_tagsDf = pd.DataFrame(get_data(3)).head(1000)
+tags_Df = pd.DataFrame(get_data(4)).head(1000)
 
 
 def find_movie(movie_name):
-    movies = moviesDf[moviesDf[movie_header[1]].str.contains(movie_name, regex=True, flags=re.IGNORECASE)]
+    movie_tagsDf = merge_tags()
+
+    movies = movie_tagsDf[movie_tagsDf[movie_header[1]].str.contains(movie_name, regex=True, flags=re.IGNORECASE)]
     return movies
 
 
-def movie_json(movie_name):
-    searched_movies = find_movie(movie_name=movie_name)
+def merge_tags():
+    # merge with genome tags and score
+    movies_scoresDf = pd.merge(moviesDf, genome_scoresDf, on=movie_header[0], how="inner")
+    movies_tagsDf = pd.merge(movies_scoresDf, genome_tagsDf, on=genome_tags_header[0])
 
+    movies_tagsDf.sort_values(inplace=True, by='relevance', ascending=False)
+
+    grouped_movies = movies_tagsDf.groupby(movie_header[0]).head(20)
+
+    return grouped_movies
+
+
+def movie_json(movie_name):
+    # find the movies based on movie_name
+    searched_movies = find_movie(movie_name=movie_name)
+    # replace | with comma in genres
     searched_movies[movie_header[2]] = searched_movies[movie_header[2]].str.replace("|", ',')
-    searched_movies[movie_header[1]] = searched_movies[movie_header[1]].str.replace("[^a-zA-Z\s]","")
-    print(searched_movies)
-    movie_meta = pd.merge(searched_movies, metaDf, on='Title', how="left")
-    json_movies = movie_meta.reset_index().to_json(orient='records')
+    # Remove the release year
+    searched_movies[movie_header[1]] = searched_movies[movie_header[1]].str.replace("[^a-zA-Z\s]", "")
+    # convert to json
+    json_movies = searched_movies.reset_index().to_json(orient='records')
     data = []
     data = json.loads(json_movies)
     movies_context = {'d': data}
@@ -58,4 +76,4 @@ def movie_json(movie_name):
 
 
 if __name__ == '__main__':
-    movie_json("Hello")
+    print(find_movie("Toy Story"))
